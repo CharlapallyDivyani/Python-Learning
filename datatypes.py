a6f47334ce9735d0b24c08778b2d8366156383e4
CharlapallyDivyani <charlapallydivyani@gmail.com>
2026-06-09 16:31:32 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+x=5
+y=3.14
+z="Hello"
+a = ["apple", "banana", "cherry"]		
+b = ("apple", "banana", "cherry")		
+c= range(6)	
+d= {"name" : "John", "age" : 36}		
+e = {"apple", "banana", "cherry"}		
+f = frozenset({"apple", "banana", "cherry"})		
+g = True		
+h = b"Hello"		
+i= bytearray(5)		
+j = memoryview(bytes(5))	
+k= None
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
