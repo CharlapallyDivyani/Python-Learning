9a57991e6965c3f4fe74c0d7f96ecb75a9ebed3c
CharlapallyDivyani <charlapallydivyani@gmail.com>
2026-06-10 19:07:24 +0530

    strings in python

--- strings.py
+++ strings.py
@@ -3,3 +3,15 @@ print(a[1])
 print(a[2])
 b="hello"
 print(len(b))
+x="abcd"
+if "a" in x:
+   print("Yes a is present in the x")
+c="hello"
+print(c[-3:-1])
+print(a.upper())
+print(a.lower())
+print(a.replace("i","j"))
+z=a+" "+b
+print(z)
+s=f"iam chinnu,iam{a}"
+print(s)
