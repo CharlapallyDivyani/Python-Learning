f635b7f149547a35b3184a99462e00a6c5e49861
CharlapallyDivyani <charlapallydivyani@gmail.com>
2026-06-10 18:46:11 +0530

    added casting,string,loops

---
+++ casting.py
@@ -0,0 +1,9 @@
+# Create an integer
+x=int(1)
+# Convert to float
+a=float(x)
+# Convert to string
+b=str(x)
+# Print values
+print(a)
+print(b)
---
+++ strings.py
@@ -0,0 +1,5 @@
+a="iam"
+print(a[1])
+print(a[2])
+b="hello"
+print(len(b))
