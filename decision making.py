119071a5d3b46cf0937d445feb4fa87374f814c2
CharlapallyDivyani <charlapallydivyani@gmail.com>
2026-06-11 18:26:25 +0530

    if-else-if

---
+++ decision making.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = int(input())
+
+# code here
+if(n<m):
+    print("less")
+else:
+    if(n==m):
+         print("equal")
+    else:
+        if(n>m):
+             print("greater")
