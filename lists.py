194be22eec01ea08b86dbdf0f2bd50130f866cf9
CharlapallyDivyani <charlapallydivyani@gmail.com>
2026-06-11 18:24:26 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,10 @@
+x=["apple","banana","cheery"]
+print(x)
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+y = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+y[1:3] = ["blackcurrant", "watermelon"]
+print(y)
+z = ["apple", "banana", "cherry"]
+z.insert(1, "orange")
+print(z)
